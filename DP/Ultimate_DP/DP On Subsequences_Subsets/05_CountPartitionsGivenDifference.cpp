0072ce4c51d403303a9b3e3bb86eb530ce0d6efc
CoderS2020 <pawarswaraj4@gmail.com>
2022-02-17 20:30:53 +0530

    DP Subsequences

---
+++ DP/Ultimate_DP/DP On Subsequences_Subsets/05_CountPartitionsGivenDifference.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+
+int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+
+    if(ind == 0){
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(target==0 || target == arr[0])
+                return 1;
+            return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken + taken;
+}
+
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return findWaysUtil(n-1,k,num,dp);
+}
+
+int main() {
+
+  vector<int> arr = {0,0,1};
+  int k=1;
+                                 
+  cout<<"The number of subsets found are " <<findWays(arr,k);
+}
+*/
+
+
+
+/*
+Time Complexity: O(N*K)
+
+Reason: There are N*K states therefore at max ‘N*K’ new problems will be solved.
+
+Space Complexity: O(N*K) + O(N)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*K)).
+int mod =(int)1e9+7;
+
+int countPartitionsUtil(int ind, int target, vector<int>& arr, vector<vector
+<int>> &dp){
+
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%mod;
+}
+
+int countPartitions(int d, vector<int>& arr){
+    int n = arr.size();
+    int totSum = 0;
+    for(int i=0; i<arr.size();i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d<0) return 0;
+    if((totSum-d)%2==1) return 0;
+    
+    int s2 = (totSum-d)/2;
+    
+    vector<vector<int>> dp(n,vector<int>(s2+1,-1));
+    return countPartitionsUtil(n-1,s2,arr,dp);
+}
+
+*/
+
+/*
+int mod =(int)1e9+7;
+
+int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken)%mod;
+        }
+    }
+    return dp[n-1][tar];
+}
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
+}
+Time Complexity: O(N*K)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*K)
+
+Reason: We are using an external array of size ‘N*K’. Stack Space is eliminated.
+*/
+
+/*
+Space Optimized
+int mod =(int)1e9+7;
+
+int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<int> prev(tar+1,0);
+    
+    if(num[0] == 0) prev[0] =2;  // 2 cases -pick and not pick
+    else prev[0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<int> cur(tar+1,0);
+        for(int target= 0; target<=tar; target++){
+            int notTaken = prev[target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+        
+            cur[target]= (notTaken + taken)%mod;
+        }
+        prev = cur;
+    }
+    return prev[tar];
+}
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
+}
+  Time Complexity: O(N*K)
+
+Reason: There are three nested loops
+
+Space Complexity: O(K)
+
+Reason: We are using an external array of size ‘K+1’ to store only one row.
+*/
+
+
---
+++ DP/Ultimate_DP/DP On Subsequences_Subsets/06_01Knapsack.cpp
@@ -0,0 +1,109 @@
+/*
+Memoization
+int knapsackUtil(vector<int>& wt, vector<int>& val, int ind, int W, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        if(wt[0] <=W) return val[0];
+        else return 0;
+    }
+    
+    if(dp[ind][W]!=-1)
+        return dp[ind][W];
+        
+    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+    
+    int taken = INT_MIN;
+    if(wt[ind] <= W)
+        taken = val[ind] + knapsackUtil(wt,val,ind-1,W-wt[ind],dp);
+        
+    return dp[ind][W] = max(notTaken,taken);
+}
+
+
+int knapsack(vector<int>& wt, vector<int>& val, int n, int W){
+    
+    vector<vector<int>> dp(n,vector<int>(W+1,-1));
+    return knapsackUtil(wt, val, n-1, W, dp);
+}
+Time Complexity: O(N*W)
+
+Reason: There are N*W states therefore at max ‘N*W’ new problems will be solved.
+
+Space Complexity: O(N*W) + O(N)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*W)).
+*/
+
+/*
+Tabulation
+int knapsack(vector<int>& wt, vector<int>& val, int n, int W){
+    
+    vector<vector<int>> dp(n,vector<int>(W+1,0));
+    
+    //Base Condition
+    
+    for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                
+            dp[ind][cap] = max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
+}
+Time Complexity: O(N*W)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*W)
+
+Reason: We are using an external array of size ‘N*W’. Stack Space is eliminated.
+*/
+
+
+/*
+Space Optimized
+int knapsack(vector<int>& wt, vector<int>& val, int n, int W){
+    
+    vector<int> prev(W+1,0);
+    
+    //Base Condition
+    
+    for(int i=wt[0]; i<=W; i++){
+        prev[i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=W; cap>=0; cap--){
+            
+            int notTaken = 0 + prev[cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + prev[cap - wt[ind]];
+                
+            prev[cap] = max(notTaken, taken);
+        }
+    }
+    
+    return prev[W];
+}
+
+Time Complexity: O(N*W)
+
+Reason: There are two nested loops.
+
+Space Complexity: O(W)
+
+Reason: We are using an external array of size ‘W+1’ to store only one row.
+*/
